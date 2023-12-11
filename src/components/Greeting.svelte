e6149be9ac6d267eb6630318e6b82d24b3d46ee2
SebastianAlba21 <juan.alba@uao.edu.co>
2023-12-11 10:35:43 -0500

    Adding React, Creating Player component

---
+++ src/components/Greeting.svelte
@@ -0,0 +1,16 @@
+<script lang="js">
+	const curremtTime = new Date()
+	const currentHour = curremtTime.getHours()
+
+	let greeting = ''
+
+	if (currentHour < 12) {
+		greeting = 'Good morning'
+	} else if (currentHour < 18) {
+		greeting = 'Good afternoon'
+	} else {
+		greeting = 'Good evening'
+	}
+</script>
+
+<h1 class="text-3xl font-bold select-none">{greeting}</h1>
