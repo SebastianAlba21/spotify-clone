ead48c5979c9da4ec3a391a1258da1ee330c4de6
SebastianAlba21 <juan.alba@uao.edu.co>
2024-01-07 14:30:19 -0500

    Se agregan los estados gloables con Zustand, se crea el Slider del volumen

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,4 +1,6 @@
-import { useState, useRef, useEffect } from 'react'
+import { usePlayerStore } from '@/store/playerStore'
+import { useRef, useEffect } from 'react'
+import { Slider } from './Slider'
 
 export const Pause = ({ className }) => (
 	<svg
@@ -53,25 +55,71 @@ export const Volume = () => (
 	</svg>
 )
 
-export function Player() {
-	const [isPlaying, setIsPlaying] = useState(false)
-	const [currentSong, setCurrentSong] = useState(null)
-	const audioRef = useRef()
-	useEffect(() => {
-		audioRef.current.src = `/music/1/01.mp3`
-	}, [])
+const CurrentSong = ({ image, title, artists }) => {
+	return (
+		<div className={`flex items-center gap-5 relative overflow-hidden`}>
+			<picture className="w-16 h-16 bg-zinc-600 rounded-md shadow-lg overflow-hidden">
+				<img
+					src={image}
+					alt={title}
+				/>
+			</picture>
+			<div className="flex flex-col">
+				<h3 className="font-semibold block text-sm">{title}</h3>
+				<span className="text-xs opacity-80">{artists?.join(', ')}</span>
+			</div>
+		</div>
+	)
+}
 
-	const handleClick = () => {
-		if (isPlaying) {
-			audioRef.current.pause()
-		} else {
+const VolumeControl = () => {
+	const volume = usePlayerStore(state => state.volume)
+	const setVolume = usePlayerStore(state => state.setVolume)
+	return (
+		<div className="flex justify-center gap-x-2">
+			{volume < 0.1 ? <VolumeSilence /> : <Volume />}
+			<Slider
+				defaultValue={[100]}
+				max={100}
+				min={0}
+				className="w-[95px]"
+				onValueChange={value => {
+					const [newVolume] = value
+					const volumeValue = newVolume / 100
+					setVolume(volumeValue)
+				}}
+			/>
+		</div>
+	)
+}
+
+export function Player() {
+	const { isPlaying, setIsPlaying, currentMusic } = usePlayerStore(state => state)
+	const audioRef = useRef()
+	const volumeRef = useRef(1)
+
+	useEffect(() => {
+		isPlaying ? audioRef.current.play() : audioRef.current.pause()
+	}, [isPlaying])
+	useEffect(() => {
+		const { song, playlist, songs } = currentMusic
+		if (song) {
+			const src = `music/${playlist?.id}/0${song.id}.mp3`
+			audioRef.current.src = src
+			audioRef.current.volume = volumeRef.current
 			audioRef.current.play()
 		}
+	}, [currentMusic])
+
+	const handleClick = () => {
 		setIsPlaying(!isPlaying)
 	}
+
 	return (
-		<div className="flex flex-row justify-between w-full px-4 z-50">
-			<div>current song</div>
+		<div className="flex flex-row justify-between w-full text-white px-4 z-50">
+			<div>
+				<CurrentSong {...currentMusic.song} />
+			</div>
 			<div className="grid place-content-center gap-4 flex-1">
 				<div className="flex justify-center">
 					<button
@@ -79,9 +127,12 @@ export function Player() {
 						onClick={handleClick}>
 						{isPlaying ? <Pause /> : <Play />}
 					</button>
+					<audio ref={audioRef} />
 				</div>
 			</div>
-			<audio ref={audioRef} />
+			<div className="grid place-content-center">
+				<VolumeControl />
+			</div>
 		</div>
 	)
 }
